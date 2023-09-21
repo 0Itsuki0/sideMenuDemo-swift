8951434c13b7d641b00ce8bac8d3fc7d0f7c63d7
000itsuki000 <itsuki.h@collabo-style.co.jp>
2023-09-22 08:00:16 +0900

    Initial Commit

---
+++ sideMenuDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sideMenuDemo
+//
+//  Created by Itsuki on 2023/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
